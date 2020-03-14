6a805adf78a06808c55cdb93b57dcb93a9e38ca3
Oyungerel Bernstein <oogiisar@gmail.com>
2020-03-14 11:38:53 -0700

    added smoke test

--- src/App.test.js
+++ src/App.test.js
@@ -2,3 +2,13 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
+it('renders without crashing', () => {
+  // first create a DOM element to render the component into
+  const div = document.createElement('div');
+
+  // render the component, this is the actual test, if something is wrong it will fail here
+  ReactDOM.render(<App />, div);
+
+  // clean up code
+  ReactDOM.unmountComponentAtNode(div);
+});
