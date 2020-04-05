65f33361a9880dc6eac485c8a314b89a1c68a90a
Oyungerel Bernstein <oogiisar@gmail.com>
2020-04-04 21:45:13 -0700

    updated landing page text to better describe app

--- src/components/Overview.js
+++ src/components/Overview.js
@@ -32,6 +32,7 @@ class Overview extends Component {
             <div className="overview">
                 <h1>Not Our Backyard</h1>
                 <h3>Keep the Community Clean and the Earth Green</h3>
+                <h4 id="mission">View how much garbage people have picked up around the world. Now it is your turn to create an account and start recording how much garbage you can pick up in your community</h4>
 
                 <section className='country_selector'>
                     <CountrySelector 
